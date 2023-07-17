160e96dc5dfd68874af2ab6b9eb13718597989b4
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-07-17 14:01:44 +0100

    Add clientServices soa call

---
+++ soa-gateway-service/src/integrationTest/java/uk/gov/laa/ccms/soa/gateway/client/ClientServicesClientIntegrationTest.java
@@ -0,0 +1,109 @@
+package uk.gov.laa.ccms.soa.gateway.client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.ws.test.client.RequestMatchers.xpath;
+import static org.springframework.ws.test.client.ResponseCreators.withError;
+import static org.springframework.ws.test.client.ResponseCreators.withPayload;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.test.client.MockWebServiceServer;
+import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbim.ClientInqRS;
+import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbio.ClientInfo;
+
+@SpringBootTest
+@ContextConfiguration
+public class ClientServicesClientIntegrationTest {
+
+    @Autowired
+    private WebServiceTemplate webServiceTemplate;
+
+    @Autowired
+    private ClientServicesClient client;
+
+    private static MockWebServiceServer mockServer;
+
+    @Value("classpath:/payload/ClientInqRS_valid.xml")
+    Resource clientInqRS_valid;
+
+    private static final String HEADER_NS = "http://legalservices.gov.uk/Enterprise/Common/1.0/Header";
+    private static final String MSG_NS = "http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIM";
+    private static final String CLIENT_NS = "http://legalservices.gov.uk/CCMS/ClientManagement/Client/1.0/ClientBIO";
+    private static final String COMMON_NS = "http://legalservices.gov.uk/Enterprise/Common/1.0/Common";
+
+    private  String testLoginId;
+    private   String testUserType;
+    private  int maxRecords;
+    private  String firstName;
+    private  String surname;
+
+    private Map<String, String> namespaces;
+
+    @BeforeEach
+    public void createServer() {
+        mockServer = MockWebServiceServer.createServer(webServiceTemplate);
+
+        testLoginId = "testLogin";
+        testUserType = "testType";
+        maxRecords = 50;
+        firstName = "John";
+        surname = "Doe";
+
+        namespaces = new HashMap<>();
+        namespaces.put("header", HEADER_NS);
+        namespaces.put("msg", MSG_NS);
+        namespaces.put("client", CLIENT_NS);
+        namespaces.put("common", COMMON_NS);
+    }
+
+    @Test
+    public void testGetClientDetails_ReturnsData() throws Exception {
+        ClientInfo clientInfo = buildClientInfo();
+
+        mockServer.expect(xpath("/msg:ClientInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
+                .andExpect(xpath("/msg:ClientInqRQ/header:HeaderRQ/header:UserLoginID", namespaces).evaluatesTo(testLoginId))
+                .andExpect(xpath("/msg:ClientInqRQ/header:HeaderRQ/header:UserRole", namespaces).evaluatesTo(testUserType))
+                .andExpect(xpath("/msg:ClientInqRQ/msg:SearchCriteria/msg:ClientInfo/client:FirstName", namespaces).evaluatesTo(firstName))
+                .andExpect(xpath("/msg:ClientInqRQ/msg:SearchCriteria/msg:ClientInfo/client:Surname", namespaces).evaluatesTo(surname))
+                .andRespond(withPayload(clientInqRS_valid));
+
+
+        ClientInqRS response = client.getClientDetails(testLoginId, testUserType,
+                maxRecords, clientInfo);
+
+        assertNotNull(response.getClientList());
+        assertEquals(2, response.getClientList().getClient().size());
+
+        mockServer.verify();
+    }
+
+    @Test
+    public void testGetClientDetails_HandlesError() {
+        ClientInfo clientInfo = buildClientInfo();
+
+        mockServer.expect(xpath("/msg:ClientInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
+                .andRespond(withError("Failed to call soap service"));
+
+        assertThrows(RuntimeException.class, () -> client.getClientDetails(
+                testLoginId, testUserType, maxRecords, clientInfo));
+
+        mockServer.verify();
+    }
+
+    private ClientInfo buildClientInfo(){
+        ClientInfo clientInfo = new ClientInfo();
+        clientInfo.setFirstName(firstName);
+        clientInfo.setSurname(surname);
+        return clientInfo;
+    }
+}
