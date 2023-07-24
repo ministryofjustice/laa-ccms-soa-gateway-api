d07ca4d9356d99a0c9363d57d3fd1c5ff13b6f6a
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-07-24 14:16:44 +0100

    Amend function to make clients into a page object

--- soa-gateway-service/src/test/java/uk/gov/laa/ccms/soa/gateway/service/ClientDetailsServiceTest.java
+++ soa-gateway-service/src/test/java/uk/gov/laa/ccms/soa/gateway/service/ClientDetailsServiceTest.java
@@ -6,12 +6,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import uk.gov.laa.ccms.soa.gateway.client.ClientServicesClient;
 import uk.gov.laa.ccms.soa.gateway.mapper.ClientDetailsMapper;
 import uk.gov.laa.ccms.soa.gateway.model.ClientDetail;
 import uk.gov.laa.ccms.soa.gateway.model.ClientDetails;
 import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbim.ClientInqRS;
 import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbio.ClientInfo;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
@@ -34,10 +40,12 @@ public class ClientDetailsServiceTest {
     private String firstName;
     private String surname;
     private String gender;
-    private String clientReferenceNumber;
+    private String caseReferenceNumber;
     private String homeOfficeReference;
     private String nationalInsuranceNumber;
     private Integer maxRecords;
+
+    private Pageable pageable;
 
 
     @BeforeEach
@@ -47,10 +55,12 @@ public class ClientDetailsServiceTest {
         this.firstName = "John";
         this.surname = "Doe";
         this.gender = "Male";
-        this.clientReferenceNumber = "1234567890";
+        this.caseReferenceNumber = "1234567890";
         this.homeOfficeReference = "ABC123";
         this.nationalInsuranceNumber = "AB123456C";
         this.maxRecords = 50;
+
+        this.pageable = Pageable.ofSize(20);
     }
     @Test
     public void testGetClientDetails() {
@@ -63,6 +73,9 @@ public class ClientDetailsServiceTest {
         ClientInfo clientInfo = buildClientInfo();
         ClientDetail clientDetail = buildClientDetail();
 
+        List<ClientDetail> clientDetailList = Collections.singletonList(clientDetail);
+        Page<ClientDetail> clientDetailPage = new PageImpl<>(clientDetailList, pageable, clientDetailList.size());
+
         when(clientDetailsMapper.toClientInfo(clientDetail)).thenReturn(clientInfo);
 
         // Stub the client to return the mocked response
@@ -74,14 +87,16 @@ public class ClientDetailsServiceTest {
                 .thenReturn(response);
 
         // Stub the mapper to return the mocked client details
-        when(clientDetailsMapper.toClientDetails(response)).thenReturn(clientDetails);
+        when(clientDetailsMapper.toClientDetailList(response)).thenReturn(clientDetailList);
+        when(clientDetailsMapper.toClientDetails(clientDetailPage)).thenReturn(clientDetails);
 
         // Call the service method
         ClientDetails result = clientDetailsService.getClientDetails(
                 soaGatewayUserLoginId,
                 soaGatewayUserRole,
                 maxRecords,
-                clientDetail
+                clientDetail,
+                pageable
         );
 
         // Verify that the client method was called with the expected arguments
@@ -92,8 +107,10 @@ public class ClientDetailsServiceTest {
                 clientInfo
         );
 
-        // Verify that the mapper method was called with the mocked response
-        verify(clientDetailsMapper).toClientDetails(response);
+        // Verify that the mapper methods were called
+        verify(clientDetailsMapper).toClientInfo(clientDetail);
+        verify(clientDetailsMapper).toClientDetailList(response);
+        verify(clientDetailsMapper).toClientDetails(clientDetailPage);
 
         // Assert the result
         assertEquals(clientDetails, result);
@@ -104,7 +121,7 @@ public class ClientDetailsServiceTest {
                 .firstName(firstName)
                 .surname(surname)
                 .gender(gender)
-                .clientReferenceNumber(clientReferenceNumber)
+                .caseReferenceNumber(caseReferenceNumber)
                 .homeOfficeReference(homeOfficeReference)
                 .nationalInsuranceNumber(nationalInsuranceNumber);
     }
@@ -115,7 +132,7 @@ public class ClientDetailsServiceTest {
         clientInfo.setSurname(surname);
         clientInfo.setNINumber(nationalInsuranceNumber);
         clientInfo.setHomeOfficeReference(homeOfficeReference);
-        clientInfo.setCaseReferenceNumber(clientReferenceNumber);
+        clientInfo.setCaseReferenceNumber(caseReferenceNumber);
         return clientInfo;
     }
 }
