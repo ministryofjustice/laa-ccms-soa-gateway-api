fbaaf8c51135b2c2bf193d7bfe4d41fa4cabb34e
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-07-24 14:16:44 +0100

    Amend function to make clients into a page object

--- soa-gateway-service/src/main/java/uk/gov/laa/ccms/soa/gateway/service/ClientDetailsService.java
+++ soa-gateway-service/src/main/java/uk/gov/laa/ccms/soa/gateway/service/ClientDetailsService.java
@@ -2,13 +2,21 @@ package uk.gov.laa.ccms.soa.gateway.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import uk.gov.laa.ccms.soa.gateway.client.ClientServicesClient;
 import uk.gov.laa.ccms.soa.gateway.mapper.ClientDetailsMapper;
+import uk.gov.laa.ccms.soa.gateway.util.PaginationUtil;
 import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbim.ClientInqRS;
 import uk.gov.laa.ccms.soa.gateway.model.ClientDetails;
 import uk.gov.laa.ccms.soa.gateway.model.ClientDetail;
 import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbio.ClientInfo;
+import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbio.ClientList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +31,8 @@ public class ClientDetailsService {
             String soaGatewayUserLoginId,
             String soaGatewayUserRole,
             Integer maxRecords,
-            ClientDetail clientDetail
+            ClientDetail clientDetail,
+            Pageable pageable
     ) {
         ClientInfo clientInfo =  clientDetailsMapper.toClientInfo(clientDetail);
 
@@ -33,7 +42,12 @@ public class ClientDetailsService {
                 maxRecords,
                 clientInfo);
 
-        return clientDetailsMapper.toClientDetails(response);
+        List<ClientDetail> clientDetailList = clientDetailsMapper.toClientDetailList(response);
+
+        Page<ClientDetail> page = PaginationUtil.paginateList(pageable, clientDetailList);
+
+        return clientDetailsMapper.toClientDetails(page);
+
     }
 
 }
