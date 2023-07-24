5f7a3cce147ece764451d6d77a6fc6f41fa4df9a
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-07-24 14:16:44 +0100

    Amend function to make clients into a page object

--- soa-gateway-service/src/main/java/uk/gov/laa/ccms/soa/gateway/mapper/ClientDetailsMapper.java
+++ soa-gateway-service/src/main/java/uk/gov/laa/ccms/soa/gateway/mapper/ClientDetailsMapper.java
@@ -2,25 +2,48 @@ package uk.gov.laa.ccms.soa.gateway.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.data.domain.Page;
 import uk.gov.laa.ccms.soa.gateway.model.ClientDetail;
 import uk.gov.laa.ccms.soa.gateway.model.ClientDetails;
 import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbim.ClientInqRS;
 import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbio.ClientInfo;
 import uk.gov.legalservices.ccms.clientmanagement.client._1_0.clientbio.ClientList;
 
+import java.util.Collections;
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ClientDetailsMapper {
 
-    @Mapping(target = "clients", source = "clientList.client")
-    ClientDetails toClientDetails(ClientInqRS clientInqRS);
+    @Mapping(target = ".", source = "clientList.client")
+    List<ClientDetail> toClientDetailList(List<ClientList> clientList);
+
+    ClientDetails toClientDetails(Page<ClientDetail> page);
+
+    default List<ClientDetail> toClientDetailList(ClientInqRS clientInqRS) {
+        if (clientInqRS.getClientList() != null){
+            ClientInqRS.ClientList clientListObject = clientInqRS.getClientList();
+
+            if (clientListObject.getClient() != null){
+                List<ClientList> clientList = clientListObject.getClient();
+
+                if (clientList != null) {
+                    return toClientDetailList(clientList);
+                }
+            }
+        }
+        return Collections.emptyList();
+    }
 
     @Mapping(target = ".", source = "name")
     @Mapping(target = "nationalInsuranceNumber", source = "NINumber")
+    @Mapping(target = "caseReferenceNumber", source = "clientReferenceNumber")
     ClientDetail toClientDetail(ClientList clientList);
 
     @Mapping(target = ".", source = "clientDetail")
     @Mapping(target = "NINumber", source = "clientDetail.nationalInsuranceNumber")
-    @Mapping(target = "caseReferenceNumber", source = "clientDetail.clientReferenceNumber")
     ClientInfo toClientInfo(ClientDetail clientDetail);
+
+
 
 }
