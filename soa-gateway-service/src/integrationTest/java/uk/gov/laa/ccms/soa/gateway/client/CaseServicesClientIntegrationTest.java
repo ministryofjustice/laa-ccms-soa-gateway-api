5e981f0e6e209097d73766f1d424143613b92683
Andrew Porritt <109674546+porritta@users.noreply.github.com>
2023-08-18 10:55:29 +0100

    story(ccls-1743): Add endpoint and controller method

--- soa-gateway-service/src/integrationTest/java/uk/gov/laa/ccms/soa/gateway/client/CaseServicesClientIntegrationTest.java
+++ soa-gateway-service/src/integrationTest/java/uk/gov/laa/ccms/soa/gateway/client/CaseServicesClientIntegrationTest.java
@@ -25,88 +25,135 @@ import uk.gov.legalservices.ccms.casemanagement._case._1_0.casebio.CaseInfo;
 @ContextConfiguration
 public class CaseServicesClientIntegrationTest {
 
-    @Autowired
-    private WebServiceTemplate webServiceTemplate;
+  @Autowired
+  private WebServiceTemplate webServiceTemplate;
 
-    @Autowired
-    private CaseServicesClient client;
+  @Autowired
+  private CaseServicesClient client;
 
-    private static MockWebServiceServer mockServer;
+  private static MockWebServiceServer mockServer;
 
-    @Value("classpath:/payload/CaseInqRS_valid.xml")
-    Resource caseInqRS_valid;
-    @Value("classpath:/payload/CaseInqRS_valid_one.xml")
-    Resource caseInqRS_valid_one;
+  @Value("classpath:/payload/CaseInqRS_valid.xml")
+  Resource caseInqRS_valid;
+  @Value("classpath:/payload/CaseInqRS_valid_one.xml")
+  Resource caseInqRS_valid_one;
 
-    private static final String HEADER_NS = "http://legalservices.gov.uk/Enterprise/Common/1.0/Header";
-    private static final String MSG_NS = "http://legalservices.gov.uk/CCMS/CaseManagement/Case/1.0/CaseBIM";
-    private static final String CLIENT_NS = "http://legalservices.gov.uk/CCMS/CaseManagement/Case/1.0/CaseBIO";
-    private static final String COMMON_NS = "http://legalservices.gov.uk/Enterprise/Common/1.0/Common";
+  private static final String HEADER_NS = "http://legalservices.gov.uk/Enterprise/Common/1.0/Header";
+  private static final String MSG_NS = "http://legalservices.gov.uk/CCMS/CaseManagement/Case/1.0/CaseBIM";
+  private static final String CLIENT_NS = "http://legalservices.gov.uk/CCMS/CaseManagement/Case/1.0/CaseBIO";
+  private static final String COMMON_NS = "http://legalservices.gov.uk/Enterprise/Common/1.0/Common";
 
-    private  String testLoginId;
-    private  String testUserType;
-    private  int maxRecords;
+  private String testLoginId;
+  private String testUserType;
+  private int maxRecords;
 
-    private Map<String, String> namespaces;
+  private Map<String, String> namespaces;
 
-    @BeforeEach
-    public void createServer() {
-        mockServer = MockWebServiceServer.createServer(webServiceTemplate);
+  @BeforeEach
+  public void createServer() {
+    mockServer = MockWebServiceServer.createServer(webServiceTemplate);
 
-        testLoginId = "testLogin";
-        testUserType = "testType";
-        maxRecords = 50;
+    testLoginId = "testLogin";
+    testUserType = "testType";
+    maxRecords = 50;
 
-        namespaces = new HashMap<>();
-        namespaces.put("header", HEADER_NS);
-        namespaces.put("msg", MSG_NS);
-        namespaces.put("client", CLIENT_NS);
-        namespaces.put("common", COMMON_NS);
-    }
+    namespaces = new HashMap<>();
+    namespaces.put("header", HEADER_NS);
+    namespaces.put("msg", MSG_NS);
+    namespaces.put("client", CLIENT_NS);
+    namespaces.put("common", COMMON_NS);
+  }
 
-    @Test
-    public void testGetCaseDetails_ReturnsData() throws Exception {
-        CaseInfo caseInfo = buildCaseInfo();
+  @Test
+  public void testGetCaseDetails_ReturnsData() throws Exception {
+    CaseInfo caseInfo = buildCaseInfo();
 
-        mockServer.expect(xpath("/msg:CaseInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
-                .andExpect(xpath("/msg:CaseInqRQ/header:HeaderRQ/header:UserLoginID", namespaces).evaluatesTo(testLoginId))
-                .andExpect(xpath("/msg:CaseInqRQ/header:HeaderRQ/header:UserRole", namespaces).evaluatesTo(testUserType))
-                .andExpect(xpath("/msg:CaseInqRQ/msg:SearchCriteria/msg:CaseInfo/client:CaseReferenceNumber", namespaces)
-                    .evaluatesTo(caseInfo.getCaseReferenceNumber()))
-                .andExpect(xpath("/msg:CaseInqRQ/msg:SearchCriteria/msg:CaseInfo/client:CaseStatus", namespaces)
-                    .evaluatesTo(caseInfo.getCaseStatus()))
-                .andRespond(withPayload(caseInqRS_valid));
+    mockServer.expect(
+            xpath("/msg:CaseInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
+        .andExpect(
+            xpath("/msg:CaseInqRQ/header:HeaderRQ/header:UserLoginID", namespaces).evaluatesTo(
+                testLoginId))
+        .andExpect(xpath("/msg:CaseInqRQ/header:HeaderRQ/header:UserRole", namespaces).evaluatesTo(
+            testUserType))
+        .andExpect(
+            xpath("/msg:CaseInqRQ/msg:SearchCriteria/msg:CaseInfo/client:CaseReferenceNumber",
+                namespaces)
+                .evaluatesTo(caseInfo.getCaseReferenceNumber()))
+        .andExpect(
+            xpath("/msg:CaseInqRQ/msg:SearchCriteria/msg:CaseInfo/client:CaseStatus", namespaces)
+                .evaluatesTo(caseInfo.getCaseStatus()))
+        .andRespond(withPayload(caseInqRS_valid));
 
+    CaseInqRS response = client.getCaseDetails(testLoginId, testUserType,
+        maxRecords, caseInfo);
 
-        CaseInqRS response = client.getCaseDetails(testLoginId, testUserType,
-                maxRecords, caseInfo);
+    assertNotNull(response.getCaseList());
+    assertEquals(10, response.getCaseList().size());
 
-        assertNotNull(response.getCaseList());
-        assertEquals(10, response.getCaseList().size());
+    mockServer.verify();
+  }
 
-        mockServer.verify();
-    }
+  @Test
+  public void testGetCaseDetails_HandlesError() {
+    CaseInfo caseInfo = buildCaseInfo();
 
-    @Test
-    public void testGetCaseDetails_HandlesError() {
-        CaseInfo caseInfo = buildCaseInfo();
+    mockServer.expect(
+            xpath("/msg:CaseInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
+        .andRespond(withError("Failed to call soap service"));
 
-        mockServer.expect(xpath("/msg:CaseInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
-                .andRespond(withError("Failed to call soap service"));
+    assertThrows(RuntimeException.class, () -> client.getCaseDetails(
+        testLoginId, testUserType, maxRecords, caseInfo));
 
-        assertThrows(RuntimeException.class, () -> client.getCaseDetails(
-                testLoginId, testUserType, maxRecords, caseInfo));
+    mockServer.verify();
+  }
 
-        mockServer.verify();
-    }
+  @Test
+  public void testGetCaseDetail_ReturnsData() throws Exception {
+    final String caseReferenceNumber = "300000195071";
 
-    private CaseInfo buildCaseInfo(){
-        CaseInfo caseInfo = new CaseInfo();
-        caseInfo.setCaseReferenceNumber("caseref");
-        caseInfo.setCaseStatus("casestatus");
-        caseInfo.setClientSurname("asurname");
-        caseInfo.setProviderCaseReferenceNumber("provcaseref");
-        caseInfo.setFeeEarnerContactID("123");
-        return caseInfo;
-    }
+    mockServer.expect(
+            xpath("/msg:CaseInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
+        .andExpect(
+            xpath("/msg:CaseInqRQ/header:HeaderRQ/header:UserLoginID", namespaces).evaluatesTo(
+                testLoginId))
+        .andExpect(xpath("/msg:CaseInqRQ/header:HeaderRQ/header:UserRole", namespaces).evaluatesTo(
+            testUserType))
+        .andExpect(
+            xpath("/msg:CaseInqRQ/msg:SearchCriteria/msg:CaseReferenceNumber",
+                namespaces)
+                .evaluatesTo(caseReferenceNumber))
+        .andRespond(withPayload(caseInqRS_valid_one));
+
+    CaseInqRS response = client.getCaseDetail(testLoginId, testUserType, caseReferenceNumber);
+
+    assertNotNull(response.getCase());
+    assertEquals(caseReferenceNumber, response.getCase().getCaseReferenceNumber());
+    assertNotNull(response.getCase().getCaseDetails());
+
+    mockServer.verify();
+  }
+
+  @Test
+  public void testGetCaseDetail_HandlesError() {
+    final String caseReferenceNumber = "300000195071";
+
+    mockServer.expect(
+            xpath("/msg:CaseInqRQ/header:HeaderRQ/header:TransactionRequestID", namespaces).exists())
+        .andRespond(withError("Failed to call soap service"));
+
+    assertThrows(RuntimeException.class, () -> client.getCaseDetail(
+        testLoginId, testUserType, caseReferenceNumber));
+
+    mockServer.verify();
+  }
+
+  private CaseInfo buildCaseInfo() {
+    CaseInfo caseInfo = new CaseInfo();
+    caseInfo.setCaseReferenceNumber("caseref");
+    caseInfo.setCaseStatus("casestatus");
+    caseInfo.setClientSurname("asurname");
+    caseInfo.setProviderCaseReferenceNumber("provcaseref");
+    caseInfo.setFeeEarnerContactID("123");
+    return caseInfo;
+  }
 }
